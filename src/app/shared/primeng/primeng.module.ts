34b4fac87cabcd82601b99945becb4caa88fc712
unknown <fatma.elarabii@gmail.com>
2022-05-30 13:15:29 -0700

    card style

--- src/app/shared/primeng/primeng.module.ts
+++ src/app/shared/primeng/primeng.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DropdownModule } from 'primeng/dropdown';
-import {CardModule} from 'primeng/card';
 
 const primengUsedModules = [
   DropdownModule,
-  CardModule
 ]
 
 @NgModule({
