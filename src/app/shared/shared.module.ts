f2e6f1afd7119b86bd5b638f66a78e0fac023de6
unknown <fatma.elarabii@gmail.com>
2022-05-29 19:40:53 -0700

    create shared components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+import { DropDownComponent } from './components/drop-down/drop-down.component';
+import { CardComponent } from './components/card/card.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DropDownComponent,
+    CardComponent
+  ],
   imports: [
     CommonModule,
     SharedRoutingModule
+  ],
+  exports: [
+    DropDownComponent,
+    CardComponent
   ]
 })
 export class SharedModule { }
