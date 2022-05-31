502934c27216c4f9832a6b72578374b5273a4f5d
unknown <fatma.elarabii@gmail.com>
2022-05-30 23:21:35 -0700

    chart style

--- src/app/weather-dashboard/components/city-forecast/city-forecast.component.ts
+++ src/app/weather-dashboard/components/city-forecast/city-forecast.component.ts
@@ -18,17 +18,47 @@ export class CityForecastComponent implements OnInit {
     ];
 
     chartOption: EChartsOption = {
+      tooltip: {
+        show: true
+      },
       xAxis: {
         type: 'category',
         data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        axisLine: {
+          lineStyle: {
+            color: "#ffff11"
+          }
+        },
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          fontWeight: "bolder"
+        }
       },
       yAxis: {
         type: 'value',
+        axisLine: {
+          show: false,
+          lineStyle: {
+            color: "#ffff11"
+          }
+        },
+        axisTick: {
+          show: false,
+        }
       },
       series: [
         {
           data: [820, 932, 901, 934, 1290, 1330, 1320],
           type: 'line',
+          itemStyle: {
+            color: "#fff"
+          },
+          areaStyle: {
+            color: "#fff",
+            opacity: 0.5
+          }
         },
       ],
     };
