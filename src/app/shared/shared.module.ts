b4f1e8e11e6abd2451fa825597f189f8bf66fe2b
unknown <fatma.elarabii@gmail.com>
2022-06-01 22:03:08 -0700

    remove related code to primeng

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,24 +3,19 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'; 
 
 import { SharedRoutingModule } from './shared-routing.module';
-import { DropDownComponent } from './components/drop-down/drop-down.component';
 import { CardComponent } from './components/card/card.component';
-import { PrimengModule } from './primeng/primeng.module';
 
 
 @NgModule({
   declarations: [
-    DropDownComponent,
     CardComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
-    SharedRoutingModule,
-    PrimengModule
+    SharedRoutingModule
   ],
   exports: [
-    DropDownComponent,
     CardComponent
   ]
 })
