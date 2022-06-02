43c5f027dcd1b2ec38433b1f8c64fb42d2917a95
unknown <fatma.elarabii@gmail.com>
2022-06-02 00:20:10 -0700

    get city weather and bind data in the card

--- src/app/weather-dashboard/components/city-forecast/city-forecast.component.ts
+++ src/app/weather-dashboard/components/city-forecast/city-forecast.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EChartsOption } from 'echarts';
+import { IList } from '../../models/city-forecast';
 import { WeatherForecastService } from '../../services/weather-forecast.service';
 
 @Component({
@@ -58,6 +59,14 @@ export class CityForecastComponent implements OnInit {
   currentCountry!: string;
   cities!: string[];
   selectedCity!: string;
+  currentCityWeather!: IList;
+  temp!: number;
+  windSpeed!: number;
+  humidity!: number;
+  weatherStatus!: string;
+  weatherIcon!: string;
+  countryCode!: string;
+  countryName!: string | undefined;
 
   constructor(
     private weatherService: WeatherForecastService,
@@ -66,18 +75,31 @@ export class CityForecastComponent implements OnInit {
     }
 
   ngOnInit(): void {
-    this.getCities();
+    // this.getCities();
   }
 
-  getCities() {
+  getCities(): void {
     const body = {
       country: this.currentCountry
     }
     this.weatherService.getCitiesByCountryCode(body).subscribe({
       next: cities => this.cities = cities,
       error: () => {},
-      complete: () => this.selectedCity = this.cities[0]
+      complete: () => {
+        this.selectedCity = this.cities[0];
+        this.getHistoricalWeather();
+      }
     })
   }
 
+  getHistoricalWeather(): void {
+    this.weatherService.getHistoryWeatherByCityName(this.selectedCity).subscribe( forecast => {
+      this.currentCityWeather = forecast[0];
+      this.temp = this.currentCityWeather.main.temp;
+      this.windSpeed = this.currentCityWeather.wind.speed;
+      this.humidity = this.currentCityWeather.main.humidity;
+      this.weatherStatus = this.currentCityWeather.weather[0].main;
+      this.weatherIcon = this.currentCityWeather.weather[0].icon;
+    });
+  }
 }
