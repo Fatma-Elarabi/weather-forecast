bad47c76b811ed23ad02ea68e56a7838f71dbf61
unknown <fatma.elarabii@gmail.com>
2022-06-04 21:54:59 -0700

    edit api URl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.openweathermap.org/data/2.5/weather',
+  apiUrl: 'https://api.openweathermap.org/data/2.5/',
   API_key: '259b0935cc5524942177da7954c9aa7d'
 };
